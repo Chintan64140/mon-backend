0607ac5d62592d7edea9afa6414ef590d8ad9ff5
Chintan64140 <chintan6414@gmail.com>
2025-12-10 10:41:15 +0530

    ddd

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 
@@ -5,11 +7,9 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
-
-
 const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({  connectionString });
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
